eb151c62f812dce6d4ebbfb0deb8c0d11e94a5fb
Shantanuu00 <106537844+Shantanuu00@users.noreply.github.com>
2023-12-01 08:04:47 +0000

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountID = 144
+let accountEmail = "Shantanu@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+
+//accountID=828 //not allowed 
+accountEmail="hc@hc.com"
+accountPassword="232233"
+accountCity="Bengaluru"
+/*Don't use var because it because of its
+block and functional scope*/
+
+console.table([accountID,accountEmail,accountCity,accountState]);
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,4 @@
+"use strict"; //treat all JS  code as newer version
+//alert(3+3)//It will give an error because we are using nodejs not browser
+
+

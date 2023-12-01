d129f679de6345ddf4047c9f2d39aac51bdf3250
Shantanuu00 <106537844+Shantanuu00@users.noreply.github.com>
2023-12-01 08:05:34 +0000

    02_dataTypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,4 +1,15 @@
 "use strict"; //treat all JS  code as newer version
 //alert(3+3)//It will give an error because we are using nodejs not browser
 
-
+let name="Shantanu"
+let age=18
+let isLoggedIn=false
+let state;
+//number=> 2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//Symbol=>unique
+//object

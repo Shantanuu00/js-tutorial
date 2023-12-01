0b56004703eec20aed8cc71b5d02e64e1ca014f7
Shantanuu00 <106537844+Shantanuu00@users.noreply.github.com>
2023-12-01 09:14:44 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,23 @@
+let score =33
+//console.log(typeof score);
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+//true=1 
+//false=0
+let isLoggedIn=1
+
+
+
+//""=>false
+//"Shantanu"=>true
+
+//let someNumber=33
+//let stringNumber=String(someNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+let value=3
+let negValue=-value
+console.log(negValue);

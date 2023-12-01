4379a66e9072f6a4444023da966763ba72e0a1d5
Shantanuu00 <106537844+Shantanuu00@users.noreply.github.com>
2023-12-01 10:03:45 +0000

    04_dataTypes_Summary

---
+++ 01_basics/04_dataTypes_Summary.js
@@ -0,0 +1,45 @@
+//primitives
+//7 types:String ,number,boolean,null,undefined,Symbol,bigInt
+
+//Non-primitives Or reference types
+//Array,Objects,Functions
+
+//const score=100
+//const scoreValue=100.3
+//const isLogged = false
+//const outsideTemp = null
+//let userEmail;
+
+const id=Symbol('123')
+const anotherEmail=Symbol('123')
+
+console.log(id===anotherEmail);
+
+//reference(Non primitives)
+//Arrays,Objects,Functions
+
+const heros=["shaktiman","naagraj","doga"];
+
+let myObj={
+    name:"shantanu",
+    age: 24,
+
+}
+
+const myFunction=function(){
+    console.log("Hello World")
+}
+
+//DataType of null is always object
+//DataType of nonprimitives is object function
+console.log(typeof heros);
+console.log(typeof myObj);
+console.log(typeof myFunction);
+//typeof function is function
+//typeof null is object
+
+
+
+
+
+

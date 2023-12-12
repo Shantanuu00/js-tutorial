f74b240d1038e132e76275a38ffe94f710f24028
Shantanuu00 <106537844+Shantanuu00@users.noreply.github.com>
2023-12-12 17:10:46 +0000

    iife

---
+++ 03_basics/iife.js
@@ -0,0 +1,89 @@
+
+//Immediately Invoked Function Expression(IIFE)
+(function chai(){
+    console.log(`DB CONNECTED`);
+})(); // ; is important 
+
+//why we use iife
+//to avoid global scope pollution problem
+//it hides the data from the global scope as it creates own scope
+//wrap function in parenthesis to immediately invoke functions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

ad219f0521a7b1b29bc8efd1d07cc22d568847ba
Shantanuu00 <106537844+Shantanuu00@users.noreply.github.com>
2023-12-13 09:09:49 +0000

    control

---
+++ 04_control_flow/one.js
@@ -0,0 +1,49 @@
+//if
+//const isUserLoggedIn=true
+//const temperature=41
+
+//if(temperature === 40){
+//console.log("less than 50");    
+//}else{
+    //console.log("temperature is greater than 50");
+//}
+
+
+//console.log("Execute");
+//<,>,<=,>=,==,!=,===,!==
+
+//const score=200
+//if(score>100){
+    //let power="fly"
+//}
+//console.log(`User power:${power}`);
+//power cannot be accesses outside the scope
+
+//multiple conditions
+//&& is for both condition is true or not
+//|| is to check if both condition is true or not
+//do switch cases for multiple cases
+
+
+//falsy values
+//false, 0,-0,BigInt,"",null,undefined,NaN
+
+//truthy values
+//"0",'false'," ",[],{},function(){}
+
+//nullish Coalescing Operator(??):null undefined    
+
+//let val1;
+//val1=5??10
+//val2=null??10
+//val3=undefined??15
+//val1=null??10??20
+
+//ternary operator 
+//condition?true:false
+
+
+
+
+
+
